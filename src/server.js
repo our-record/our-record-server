b4187c6d24b6dc945b1971e84c716f89342ac845
Jay Park <devpark.0x1c@gmail.com>
2021-07-18 13:50:48 +0900

    Add: Initial Setting

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 const app = express();
 
 const handleListening = () => console.log(`${PORT}번 포트를 사용하여 서버 가동중..`);
+const handleHome = (req, res) => res.send('Home');
 
-app.get('/', (req, res) => res.send('Home'));
+app.use(morgan('dev'));
+app.get('/', handleHome);
+
 app.listen(PORT, handleListening);
