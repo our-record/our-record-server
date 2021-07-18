01f3d321a7b4eea6ef53b6d129a3df6d512216da
Jay Park <devpark.0x1c@gmail.com>
2021-07-18 12:09:31 +0900

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
-const app = express();
 const PORT = 4000;
-
-app.get('/', (req, res) => res.send('Home'));
+const app = express();
 
 const handleListening = () => console.log(`${PORT}번 포트를 사용하여 서버 가동중..`);
+
+app.get('/', (req, res) => res.send('Home'));
 app.listen(PORT, handleListening);
