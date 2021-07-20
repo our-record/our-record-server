0dd35838083be9d14460fbdfba85096ce8a08411
Jay Park <devpark.0x1c@gmail.com>
2021-07-20 14:04:34 +0900

    Add: Change Structure and Modify & Create Model

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
-import './db';
-import './models/Post';
 import express from 'express';
 import morgan from 'morgan';
 import globalRouter from './routers/globalRouter';
 import postRouter from './routers/postRouter';
 import userRouter from './routers/userRouter';
 
-const PORT = 4000;
 const app = express();
-
-const handleListening = () => console.log(`${PORT}번 포트를 사용하여 서버 가동중..`);
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -18,4 +13,4 @@ app.use('/', globalRouter);
 app.use('/post', postRouter);
 app.use('/users', userRouter);
 
-app.listen(PORT, handleListening);
+export default app;
