ca0cca64dc456b8889608d2795c21619f35cf76f
Jay Park <devpark.0x1c@gmail.com>
2021-08-11 10:07:21 +0900

    Add: Initial setting of chart page

---
+++ src/controllers/chartController.js
@@ -0,0 +1,11 @@
+export const chartHome = (req, res) => {
+  res.send('Chart');
+};
+
+export const weeklyData = (req, res) => {
+  res.send('weekly');
+};
+
+export const monthlyData = (req, res) => {
+  res.send('monthly');
+};
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import globalRouter from './routers/globalRouter';
 import postRouter from './routers/postRouter';
 import userRouter from './routers/userRouter';
 import { localsMiddleware } from './middlewares';
+import chartRouter from './routers/chartRouter';
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use(
 app.use('*/images', express.static('public/images'));
 app.use('/', globalRouter);
 app.use('/anniversary', anniversaryRouter);
+app.use('/chart', chartRouter);
 app.use('/post', postRouter);
 app.use('/user', userRouter);
 
