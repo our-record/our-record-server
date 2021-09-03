5a80c56424f3b0e789a88d5895c82730b00ab2e4
Jay Park <devpark.0x1c@gmail.com>
2021-09-03 20:17:49 +0900

    Fix: Calculate Date

--- src/routers/chartRouter.js
+++ src/routers/chartRouter.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { chartHome, monthlyData, weeklyData } from '../controllers/chartController';
+import { chartHome, monthlyData } from '../controllers/chartController';
 
 const chartRouter = express.Router();
 
 chartRouter.get('/', chartHome);
-chartRouter.post('/weekly', weeklyData);
 chartRouter.post('/monthly', monthlyData);
 
 export default chartRouter;
